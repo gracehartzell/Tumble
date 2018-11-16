7086a994ebc31f6b73a431a710cfc62b916cdd6b
Grace Hartzell <gracehartzell@utexas.edu>
2018-11-16 10:57:17 -0600

    oops

--- config/aws.js
+++ config/aws.js
@@ -1,6 +1,6 @@
 const aws = {
-	secretKey: 'pK+Ftjg1Ecyq/ZBmFZ4SuYZf3mk6VbwtFa47IwWm',
-	accessKey: 'AKIAI6NEKTPTVW2APK3A'
+	secretKey: '[YOUR SECRET KEY],
+	accessKey: '[YOUR ACCESS KEY]'
 }
 
 module.exports = aws
